99fd19541d1ca672e9a5dc07710eaace15adb674
Damien Whitten <dw@chaos404.com>
2020-07-07 18:20:37 +1000

    init

---
+++ err.go
@@ -0,0 +1,54 @@
+package protoerr
+
+import (
+	"fmt"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type protoValidationError interface {
+	Field() string
+	Reason() string
+	Cause() error
+	Key() bool
+	Error() string
+}
+
+type ValidationError struct {
+	Field  string
+	Reason string
+}
+
+func (ve ValidationError) Error() string {
+	return fmt.Sprintf("invalid %s: %s", ve.Field, ve.Reason)
+}
+
+func (ve ValidationError) GRPCStatus() *status.Status {
+	return status.New(codes.InvalidArgument, ve.Error())
+}
+
+func FormatValidationError(err error) error {
+	ve, ok := err.(protoValidationError)
+	if !ok {
+		return err
+	}
+
+	if cause := ve.Cause(); cause != nil {
+		if causeRaw, ok := cause.(protoValidationError); ok {
+			cause := FormatValidationError(causeRaw)
+			if subve, ok := cause.(*ValidationError); ok {
+				return &ValidationError{
+					Field:  ve.Field() + subve.Field,
+					Reason: subve.Reason,
+				}
+			}
+			return cause
+		}
+	}
+
+	return &ValidationError{
+		Field:  ve.Field(),
+		Reason: ve.Reason(),
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/embedfi/protoerror
+
+go 1.14
+
+require google.golang.org/grpc v1.30.0
