62efb3c7371cfd1d81d483fd0e5a405ddeb69c87
Damien Whitten <dw@chaos404.com>
2020-10-30 13:23:09 +1100

    gRPC interceptor

--- err.go
+++ err.go
@@ -1,8 +1,10 @@
 package protoerror
 
 import (
+	"context"
 	"fmt"
 
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -50,5 +52,27 @@ func FormatValidationError(err error) error {
 	return &ValidationError{
 		Field:  ve.Field(),
 		Reason: ve.Reason(),
+	}
+}
+
+type validator interface {
+	Validate() error
+}
+
+func UnaryServerInterceptor() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		if v, ok := req.(validator); ok {
+			if err := v.Validate(); err != nil {
+				return nil, FormatValidationError(err)
+			}
+		}
+		res, err := handler(ctx, req)
+		if err != nil {
+			if _, ok := status.FromError(err); !ok {
+				return nil, status.Error(codes.Internal, "Internal Error")
+			}
+			return nil, err
+		}
+		return res, nil
 	}
 }
